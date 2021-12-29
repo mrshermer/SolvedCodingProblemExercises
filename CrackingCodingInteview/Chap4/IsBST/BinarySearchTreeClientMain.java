740327f26b6cbc7a7f4e5c3b5402c8b1268c5876
Kyle Shermer <kylesherm@hotmail.com>
2021-12-29 10:39:10 -0500

    Java Code for checking weather tree is BST

---
+++ CrackingCodingInteview/Chap4/IsBST/BinarySearchTreeClientMain.java
@@ -0,0 +1,7 @@
+public class BinarySearchTreeClientMain{
+   public static void main(String[] args){
+       BinarySearchTreeValidation bst = new BinarySearchTreeValidation();
+       bst.isBinarySearchTree();
+   }
+
+}
