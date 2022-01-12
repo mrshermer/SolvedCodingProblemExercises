63afe35ea2113c91ecdd7beeef12e9893ce7cfbd
Kyle Shermer <kylesherm@hotmail.com>
2022-01-12 16:44:49 -0500

    Solution for CommonAncestorProblem

---
+++ CrackingCodingInteview/Chap4/FirstCommonAncestor/FirstCommonAncestorMain.java
@@ -0,0 +1,8 @@
+public class FirstCommonAncestorMain
+{
+   public static void main(String[] args){
+      FirstCommonAncestor fC = new FirstCommonAncestor();
+      fC.firstCommonAncestor(4, 5);
+   }
+
+}
